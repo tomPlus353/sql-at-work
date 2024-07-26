cbc87e21209daa741e5d46f757da79469d7f6256
Tomas Sullivan <t-sullivan@grune.co.jp>
2024-07-26 14:30:56 +0900

    add selector for all location data

---
+++ select_all_location_data_taberuba.sql
@@ -0,0 +1,4 @@
+SELECT p.prefecture_id, p.city_id, p.location, p.geolonia_pref, p.geolonia_city, p.geolonia_town, p.geolonia_addr FROM  p where p.prefecture_id = 4;
+
+SELECT p.prefecture_id, p.city_id, p.location, p.geolonia_pref, p.geolonia_city, p.geolonia_town, p.geolonia_addr FROM properties p where p.prefecture_id = 4;
+
